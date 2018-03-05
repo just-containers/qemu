0086a069fcfd11122ed1753833f1b53cb18a513c
John Regan <jregan@mesonet.org>
2018-03-05 09:16:57 -0500

    import musl-F_SHLCK-and-F_EXLCK.patch from voidlinux

--- linux-user/syscall.c
+++ linux-user/syscall.c
@@ -117,6 +117,13 @@ int __clone2(int (*fn)(void *), void *child_stack_base,
 
 #include "qemu.h"
 
+#ifndef F_SHLCK
+#define F_SHLCK 8
+#endif
+#ifndef F_EXLCK
+#define F_EXLCK 4
+#endif
+
 #ifndef CLONE_IO
 #define CLONE_IO                0x80000000      /* Clone io context */
 #endif
